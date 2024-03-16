e007d79b7be6673b2f2fa53d5fa03b9e267dfeab
ddtechdd <ravichandran1968@gmail.com>
2024-03-16 21:24:01 +0530

    array

--- app.js
+++ app.js
@@ -14,10 +14,21 @@ console.log(num)//10
 // Data types
 
 let myNumber;
-console.log(myNumber) // undefined
+// console.log(myNumber) // undefined
 
 let boolValue = true
 let nullValue = null
 
 console.log(boolValue)
 console.log(nullValue)
+
+
+// Array
+
+let intergerArray =[2,5,7,9,11]
+
+console.log(intergerArray.length)
+console.log(intergerArray.[0])
+console.log(intergerArray[1])
+
+console.log(intergerArray[intergerArray.length-1])
